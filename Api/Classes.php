2d680993701c7acd01aa03716917c2f79c00f2e9
Gabriel Buentello <galfrano@gmail.com>
2023-03-31 14:04:54 +0200

    post corrections, delete tested

--- Api/Classes.php
+++ Api/Classes.php
@@ -15,7 +15,7 @@ class Classes extends Rest{
   }
   protected function get($id){
     $users = (new Entity('users'))->catalog(function($u){return Common::getName($u);});
-    return $this->entity->get(1, [], true, function($line)use($users){
+    return $this->entity->get($id, [], true, function($line)use($users){
       $attendees = array_values((new Entity('attendees'))->getList(1, ['class'=>$line['_id']], function($a)use($users){return [$a['attendee']=>$users[$a['attendee']]];}));
       return [$line['_id']=>$line+['created_by_name'=>$users[$line['created_by']], 'attendees'=>$attendees]];
     });
--- Api/Rest.php
+++ Api/Rest.php
@@ -5,19 +5,20 @@ use \Model\Entity;
 abstract class Rest{
   protected $tableName;
   protected $entity;
+  protected $data;
   function __construct(){
     $this->entity = new Entity($this->tableName);
+    $this->data = json_decode(file_get_contents('php://input'), true);
   }
   
   function route($id){
   $method = $_SERVER['REQUEST_METHOD'];
     if($method == 'GET'){
-      return empty($id) ? $this->getList() : $this->get($id);
+      return empty($id) ? $this->entity->getAll() : $this->entity->get($id);
     }
     elseif($method == 'POST'){
-      $json = $this->getData();
       try{
-        return $this->entity->get($this->entity->add($json));
+        return $this->entity->get($this->entity->add($this->data));
         }
       catch(\PDOException $e){
         http_response_code(400);
@@ -25,29 +26,22 @@ abstract class Rest{
       }
     }
     elseif($method == 'PUT' && !empty($id)){
-      $json = $this->getData();
-      var_dump($json);
-      $this->entity->edit($id, $json);
-      return $this->get($id);
+      try{
+        $this->entity->edit($this->data, $id);
+        return $this->get($id);
+      }
+      catch(\PDOException $e){
+        http_response_code(400);
+        return ['message' => $e->getMessage()];
+      }
     }
     elseif($method == 'DELETE' && !empty($id)){
-      return delete($id);
+      $id == 'deleteall' ? $this->entity->deleteAll() : $this->entity->delete($id);
+      return ["acknowledged"=> true, 'deletedCount' => 1];
     }
     else{
+      http_response_code(400);
       return ['message'=>'Method not supported or missing id'];
     }
-  }
-  protected function getList(){
-    return $this->entity->getAll();
-  }
-  protected function get($id){
-    return $this->entity->get($id);
-  }
-  protected function delete($id){
-    $id == 'deleteall' ? $this->entity->deleteAll() : $this->entity->delete($id);
-    return ;//?
-  }
-  protected function getData(){
-    return json_decode(file_get_contents('php://input'), true);
   }
 }
